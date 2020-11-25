78b3e8031ebf16154ddfaa3c9856e633fa3d9bc7
Hubitskiy <dmitry.g@upservice.io>
2020-11-25 22:39:12 +0300

    fix

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 
 from users.presenters import CreateUserPresenter
 
@@ -11,8 +11,7 @@ router = APIRouter()
 @router.post(
     path='/users/',
     tags=["Users"],
-    status_code=201,
-    response_model=Dict
+    status_code=status.HTTP_204_NO_CONTENT
 )
 def create_user(create_user_presenter: CreateUserPresenter = Depends(CreateUserPresenter)) -> Dict:
     return create_user_presenter()
