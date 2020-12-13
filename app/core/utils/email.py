ad7aa8390338f3b68bf0c2ce3cf4f20a439f3bdd
Hubitskiy <dmitry.g@upservice.io>
2020-12-13 23:13:31 +0300

    email sender

--- app/core/utils/email.py
+++ app/core/utils/email.py
@@ -1,6 +1,10 @@
 import smtplib
+import ssl
+
 
 from core.settings import settings
+from celery_tasks import app
+from core.settings.settings import (SMTP_SERVER, PORT, SENDER_EMAIL)
 
 
 class EmailSender(smtplib.SMTP_SSL):
@@ -11,3 +15,16 @@ class EmailSender(smtplib.SMTP_SSL):
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.quit()
+
+
+@app.task
+def send_email(to_email: str):
+
+    context = ssl.create_default_context()
+
+    with EmailSender(host=SMTP_SERVER, port=PORT, context=context) as sender:
+        sender.sendmail(
+            from_addr=SENDER_EMAIL,
+            to_addrs=to_email,
+            msg="Test Content"
+        )
